de11316f8e11aa3fbe7954b32d51b277798a38a2
Amit Kumar <amit8.88@gmail.com>
2014-06-25 01:09:28 +0530

    Moved inline javascript into a file of its own.

---
+++ script/callFancyBox.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    /*
+     *  Open fancybox with the survey url
+     */
+
+    $.fancybox({
+        openEffect  : 'none',
+        closeEffect : 'none',
+        modal: false,
+        width:600,
+        height:500,
+        type: 'iframe',
+        href:'http://amitk.in/lime/index.php/143991/newtest/Y/lang/en'
+
+    });
+
+
+});
